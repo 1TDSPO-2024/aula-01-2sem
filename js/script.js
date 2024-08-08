cd2fda10d2f17c9f216d4c0ef3c72399fb0dedd0
Dametto98 <caikedametto98@gmail.com>
2024-08-07 22:51:25 -0300

    commit botoes + funcoes

--- js/script.js
+++ js/script.js
@@ -25,4 +25,41 @@ console.log("============LOOP FOR each");
 // frutas.forEach(function (f, indice, array) {
 //     console.log(array[indice]);
 // });
+//Arrow function = mesma coisa que a function, mantendo o codigo mais limpo e facíl de idenficar 
 frutas.forEach((f, indice, array) => console.log(array[indice]))
+
+//Recuperando a lista UL
+let lista = document.getElementById("lista");
+
+function renderizaLista() {
+    lista.innerHTML = "";
+
+    //Botao de adicionar fruta ao inicio da lista
+    document.getElementById("btnAddInicio").addEventListener("click", () => {
+        let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+        frutas.unshift("valorDigitadoNoCampo");
+        renderizaLista();
+
+    })
+
+    //Botao de adicionar fruta ao final da lista
+    document.getElementById("btnAddFinal").addEventListener("click", () => {
+        let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+        frutas.unshift("valorDigitadoNoCampo");
+        renderizaLista();
+    })
+    document.getElementById("btnAddOrdem").addEventListener("click", () => {
+        frutas.sort();
+        renderizaLista();
+    })
+    document.getElementById("btnAddReverse").addEventListener("click", () => {
+        frutas.reverse();
+        renderizaLista();
+    })
+
+    //Criando elementos li para inserir na lista 
+    frutas.forEach(fruta => {
+        let elementoLi = document.createElement("li");
+        elementoLi.textContent = fruta;
+        lista.appendChild(elementoLi);
+    }}
