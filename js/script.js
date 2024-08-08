e4407ce0cc1be0dd6baf854feb11cb0c8b9e36dc
Oscar.Arias.Neto <kazinhoarias9@gmail.com>
2024-08-07 22:51:39 -0300

    Botão resenha

--- js/script.js
+++ js/script.js
@@ -25,3 +25,37 @@ frutas.forEach(function (f, indice, array) {
     console.log(array[indice]);
 });
 frutas.forEach((f, indice, array) => console.log(array[indice]));
+
+
+document.getElementById("btnAddInicio").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.unshift(valorDigitadoNoCampo);
+    renderizaLista();
+});
+
+document.getElementById("btnAddFinal").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.push(valorDigitadoNoCampo);
+    renderizaLista();
+});
+
+document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort();
+    renderizaLista();
+});
+
+document.getElementById("btnReverse").addEventListener("click", () => {
+    frutas.reverse();
+    renderizaLista();
+});
+
+function renderizaLista() {
+    lista.innerHTML = "";
+    frutas.forEach(fruta => {
+        let elementoLi = document.createElement("li");
+        elementoLi.textContent = fruta;
+        lista.appendChild(elementoLi);
+    });
+}
+
+renderizaLista();
