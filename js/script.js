bc220aff65c72184a28dd2658f5cd2be740dd7e6
BrennoVilela <brenno.cauet@gmail.com>
2024-08-08 21:33:05 -0300

    ajuste

--- js/script.js
+++ js/script.js
@@ -1,49 +1,65 @@
+
 //Declarando um Array de Frutas
 let frutas = ["Zimbro", "Lichia", "Jambo", "Figo", "Damasco", "Pitaia"];
-//Visualizador o array
-console.log(frutas)
-console.table(frutas)
+//Visualizar o array:
+console.log(frutas);
+console.table(frutas);
 
+//Percorrer arrays com LOOPS FOR...
 
-//Percorrer arrays com loop for
+console.log("================LOOP FOR");
 for (let x = 0; x < frutas.length; x++) {
-    const element = frutas[x];
     console.log(frutas[x]);
 }
 
-//Percorrer arrays com loop for of
-for (let i of frutas) {
-    console.log(frutas);
+console.log("================LOOP FOR OF");
+for (let fruta of frutas) {
+    console.log(fruta);
 }
 
-//Percorrer arrays com loop for in
-for (let i in frutas) {
-    console.log(i, frutas[i]);
+console.log("================LOOP FOR IN");
+for (let indice in frutas) {
+    console.log(indice, frutas[indice]);
 }
 
-//Percorrer arrays com lopp for each
-// frutas.forEach(function (f, i, array) {
-//     console.log(i, f);
+console.log("================LOOP FOR EACH");
+// frutas.forEach(function (f, indice, array) {
+//     console.log(array[indice]);
 // });
+frutas.forEach((f, indice, array) => console.log(array[indice]));
 
-//for each com arrow function
-frutas.forEach((f, i, array) => console.log(array[i]));
-
-//Recuperando a lista
+//Recuperando a lista UL
 let lista = document.getElementById("lista");
 
-
-//Botão de adicionar fruta ao final da lista
-document.getElementById("btnAdd").addEventListener("click", () => {
-    let valor = document.getElementById("idFruta").value;
-    frutas.push(valor);
+//Botão de adicionar fruta ao início da lista.
+document.getElementById("btnAddInicio").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.unshift(valorDigitadoNoCampo);
     renderizaLista();
-})
+});
+
+//Botão de adicionar fruta ao final da lista.
+document.getElementById("btnAddFinal").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.push(valorDigitadoNoCampo);
+    renderizaLista();
+});
+
+document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort();
+    renderizaLista();
+});
+
+document.getElementById("btnReverse").addEventListener("click", () => {
+    frutas.reverse();
+    renderizaLista();
+});
+
 
 function renderizaLista() {
     lista.innerHTML = "";
     frutas.forEach(fruta => {
-        //Criando elementos li para inserir na lista
+        //Criando elementos li para inserir na lista.
         let elementoLi = document.createElement("li");
         elementoLi.textContent = fruta;
         lista.appendChild(elementoLi);
