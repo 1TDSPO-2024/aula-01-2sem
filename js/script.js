0a39aaa2f5590f5fc4848d9fd08d4ed633b1e958
raphatatto <raphaelatatto@gmail.com>
2024-08-08 21:25:55 -0300

    atualizando

--- js/script.js
+++ js/script.js
@@ -1,54 +1,70 @@
-// DECLARANDO UM ARRAY DE FRUTAS
-let frutas = ["Lichia", "Figo", "Pitaya", "Damasco", "Atemoia", "Laranja"]
 
-// VISUALIZAR O ARRAY
+//Declarando um Array de Frutas
+let frutas = ["Zimbro", "Lichia", "Jambo", "Figo", "Damasco", "Pitaia"];
+//Visualizar o array:
 console.log(frutas);
-console.log(frutas);
+console.table(frutas);
 
-// PERCORRER  ARRAYS COM LOOPS FOR
-console.log("=================LOOP FOR");
+//Percorrer arrays com LOOPS FOR...
+
+console.log("================LOOP FOR");
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
-
 }
-console.log("=================LOOP FOR OFF");
-for (let indice of frutas) {
+
+console.log("================LOOP FOR OF");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("================LOOP FOR IN");
+for (let indice in frutas) {
     console.log(indice, frutas[indice]);
 }
 
-console.log("=================LOOP FOR EACH");
+console.log("================LOOP FOR EACH");
 // frutas.forEach(function (f, indice, array) {
-//     console.log(indice, f);
+//     console.log(array[indice]);
 // });
 frutas.forEach((f, indice, array) => console.log(array[indice]));
 
-//RECUPERANDO A LISTA UL
+//Recuperando a lista UL
 let lista = document.getElementById("lista");
 
-//BOTÃP DE ADICIONAR FRUTA AO INICIO DA LISTA.
+//Botão de adicionar fruta ao início da lista.
 document.getElementById("btnAddInicio").addEventListener("click", () => {
     let valorDigitadoNoCampo = document.getElementById("idFruta").value;
-    frutas.push(valorDigitadoNoCampo);
+    frutas.unshift(valorDigitadoNoCampo);
     renderizaLista();
 });
 
-
-//BOTÃO DE ADICIONAR FRUTA AO FINAL DA LISTA.
+//Botão de adicionar fruta ao final da lista.
 document.getElementById("btnAddFinal").addEventListener("click", () => {
     let valorDigitadoNoCampo = document.getElementById("idFruta").value;
     frutas.push(valorDigitadoNoCampo);
     renderizaLista();
 });
 
+document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort();
+    renderizaLista();
+});
+
+document.getElementById("btnReverse").addEventListener("click", () => {
+    frutas.reverse();
+    renderizaLista();
+});
+
+
 function renderizaLista() {
     lista.innerHTML = "";
     frutas.forEach(fruta => {
-        //CRIANDO ELEMENTOS LI PARA INSERIR NA LISTA 
+        //Criando elementos li para inserir na lista.
         let elementoLi = document.createElement("li");
         elementoLi.textContent = fruta;
         lista.appendChild(elementoLi);
     });
-
 }
 
 renderizaLista();
+
