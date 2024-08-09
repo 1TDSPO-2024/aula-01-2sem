e48862e7cb79bc3290bded908516524c511572ec
JulioDev01 <juliocesarmaciel21@gmail.com>
2024-08-08 22:19:58 -0300

    Percorrendo Arrays

--- js/script.js
+++ js/script.js
@@ -28,21 +28,39 @@ frutas.forEach(function (f, indice, array) {
     console.log(indice, f);
 });
 
+
 //Recuperando a lista UL
 let lista = document.getElementById("lista");
 
-
-//Botão de adicionar fruta ao final da lista
-document.getElementById("btnAdd").addEventListener("click", () => {
-    let frutaAdicionada = document.getElementById("idFruta").value;
-    frutas.push(frutaAdicionada);
+//Botão de adicionar fruta ao início da lista.
+document.getElementById("btnAddInicio").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.unshift(valorDigitadoNoCampo);
     renderizaLista();
-})
+});
+
+//Botão de adicionar fruta ao final da lista.
+document.getElementById("btnAddFinal").addEventListener("click", () => {
+    let valorDigitadoNoCampo = document.getElementById("idFruta").value;
+    frutas.push(valorDigitadoNoCampo);
+    renderizaLista();
+});
+
+document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort();
+    renderizaLista();
+});
+
+document.getElementById("btnReverse").addEventListener("click", () => {
+    frutas.reverse();
+    renderizaLista();
+});
+
 
 function renderizaLista() {
     lista.innerHTML = "";
     frutas.forEach(fruta => {
-        //Criando elementos LI para incluir na lista
+        //Criando elementos li para inserir na lista.
         let elementoLi = document.createElement("li");
         elementoLi.textContent = fruta;
         lista.appendChild(elementoLi);
